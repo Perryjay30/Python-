0c0ba742be21942e5036eb6b721b2674b03343c0
Perryjay30 <pelumijsh@gmail.com>
2022-12-19 13:21:41 -1200

    dict_exercise.py

---
+++ Chapter9Challenge/dict_exercise.py
@@ -0,0 +1,32 @@
+# captains = {
+#     'Enterprise': 'Picard',
+#     'Voyager': 'Janeway',
+#     'Defiant': 'Sisko'
+# }
+
+captain = (
+    ('Enterprise', 'Picard'),
+    ('Voyager', 'Janeway'),
+    ('Defiant', 'Sisko')
+)
+
+captain_dict = dict(captain)
+print(captain_dict)
+
+
+
+# if 'Enterprise' in captains:
+#     print(f"The captain of Enterprise is {captains['Enterprise']}")
+# if 'Discovery' in captains:
+#     print(f"The captain of Discovery is {captains['Discovery']}")
+# else:
+#     print("unknown")
+#
+# for ship in captains:
+#     print(f"The captain of {ship} is {captains[ship]}")
+
+# del captains['Discovery']
+# print(captains)
+
+
+
